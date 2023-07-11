ce55cca1334236959b4f810f9671d6ba71081c07
Acus <acus1304pgphcm@gmail.com>
2023-07-11 09:38:17 +0700

    Phuc | Add | Linkedlist

---
+++ structures/main2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+struct person {
+     int age;
+     float weight;
+};
+
+int main() {
+     struct person *personPtr, person1;
+     personPtr = &person1;   
+
+     printf("Enter age: ");
+     scanf("%d", &personPtr->age); // (*personPtr).age
+
+     printf("Enter weight: ");
+     scanf("%f", &personPtr->weight);
+
+     printf("Displaying:\n");
+     printf("Age: %d\n", personPtr->age);
+     printf("weight: %f", personPtr->weight);
+
+}
